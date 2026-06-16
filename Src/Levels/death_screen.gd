392d8b5de1fd752bab92197e635cdaa0c0e514e6
Lucas-Code27 <83315414+Lucas-Code27@users.noreply.github.com>
2026-06-16 15:54:18 -0700

    fixed pause keybind and final controller issue

--- Src/Levels/death_screen.gd
+++ Src/Levels/death_screen.gd
@@ -4,6 +4,8 @@ extends Control
 func _ready() -> void:
 	$Label2.text = "Score: " + str(ScoreHandler.get_endscore())
 	$Label3.text = "High Score: " + str(ScoreHandler.get_highscore())
+	$Button.grab_focus()
+
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Src/Levels/Saloon.tscn")
