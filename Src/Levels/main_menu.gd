55a119181e8c90a4bf83ee56a2264615b2cb0c63
Lucas-Code27 <83315414+Lucas-Code27@users.noreply.github.com>
2026-06-17 20:41:46 -0700

    made sliders look nicer and added sand to exterior to make it not look like just a skybox

--- Src/Levels/main_menu.gd
+++ Src/Levels/main_menu.gd
@@ -3,9 +3,13 @@ extends Control
 @export var game_scene:PackedScene
 
 @onready var play: Button = $Play
+@onready var quit: Button = $Quit
 
 func _ready() -> void:
 	play.grab_focus()
+	
+	if OS.has_feature("web"):
+		quit.hide()
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_packed(game_scene)
